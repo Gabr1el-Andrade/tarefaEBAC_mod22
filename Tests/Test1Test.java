2b738fab39e6083cce7b680c8aa00a7685e715e8
gabriel andrade <tisco09090@gmail.com>
2023-12-12 09:36:33 -0400

    segundo commit

---
+++ Tests/Test1Test.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1Test {
+@Test
+    public void Test(){
+    LeitorLista.main(null);
+
+    assertTrue(LeitorLista.temMulheres());
+
+ }
+}
--- src/LeitorLista.java
+++ src/LeitorLista.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 
-public class LeitorLista {public static void main(String[] args) {
+public class LeitorLista {
+
+    public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    List<String> listaNomesSexos = new ArrayList<>();
+    List<String> listNSex = new ArrayList<>();
 
     System.out.println("Digite os nomes e sexos separados por vírgula (digite 'fim' para encerrar):");
 
@@ -18,15 +19,19 @@ public class LeitorLista {public static void main(String[] args) {
             break;
         }
 
-        listaNomesSexos.add(entrada);
+        listNSex.add(entrada);
     }
 
     scanner.close();
-    List<String> mulheres = listaNomesSexos.stream()
+    List<String> mulheres = listNSex.stream()
             .filter(nomeSexo -> nomeSexo.endsWith((",F")))
             .toList();
 
     System.out.println("Mulheres na lista:");
     mulheres.forEach(System.out::println);
 }
+
+    public static boolean temMulheres() {
+        return false;
+    }
 }
